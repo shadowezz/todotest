068d7aead97b7a49be32273052c807c5acd4a69d
shadowezz <wenjie.cao@hotmail.com>
2020-01-04 23:16:15 +0800

    Allow toggling of search between title and category

--- app/javascript/components/Search.jsx
+++ app/javascript/components/Search.jsx
@@ -3,16 +3,25 @@ import React from "react";
 class Search extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            searchBy: "title"
+        }
         this.handleChange = this.handleChange.bind(this)
+        this.changeSearch = this.changeSearch.bind(this)
+    }
+
+    changeSearch(event) {
+        this.setState({searchBy: event.target.value})
     }
 
     handleChange(event) {
         let newList = []
         if (event.target.value !== "") {
             newList = this.props.todos.filter(item => {
-                const title = item.title.toLowerCase()
+                const parameter = this.state.searchBy === "title" ? item.title.toLowerCase()
+                    : item.category.toLowerCase()
                 const filter = event.target.value.toLowerCase()
-                return title.includes(filter)
+                return parameter.includes(filter)
             })
         }
         else {
@@ -21,10 +30,16 @@ class Search extends React.Component {
         this.props.updateDisplay(newList)
     }
 
+
+
+
     render() {
         return (
             <div>
                 <input type="text" onChange={this.handleChange} placeholder="Search by title"/>
+                <input type="radio" name="search" value="title" onClick={this.changeSearch} 
+                    defaultChecked/> Title
+                <input type="radio" name="search" value="category" onClick={this.changeSearch}/> Cateogry
             </div>
         )
     }
