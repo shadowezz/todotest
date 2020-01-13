d8fee168f39f2b38856042c67a20fc2c8961a552
shadowezz <wenjie.cao@hotmail.com>
2020-01-13 13:53:18 +0800

    add javascript

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -4,6 +4,7 @@
 
 import React from 'react'
 import { render } from "react-dom";
+import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
