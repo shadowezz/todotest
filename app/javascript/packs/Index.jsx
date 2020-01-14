c83bf103beb64577a11f83aa0023448eba31179f
shadowezz <wenjie.cao@hotmail.com>
2020-01-14 11:44:31 +0800

    bootstrap

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -5,10 +5,11 @@
 import React from 'react'
 import { render } from "react-dom";
 import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'stylesheets/application'
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+//import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
 
 
