487c6d1a2f9c77a4512354cfaf3991fc375cdfe6
shadowezz <wenjie.cao@hotmail.com>
2020-01-14 18:10:23 +0800

    rm styling files

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -4,8 +4,6 @@
 
 import React from 'react'
 import { render } from "react-dom";
-//import 'bootstrap'
-//import 'stylesheets/application'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
