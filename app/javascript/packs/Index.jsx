4158df28d28dbf3e0e83efca11211faff3280f00
shadowezz <wenjie.cao@hotmail.com>
2020-01-14 19:00:26 +0800

    styling

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -4,6 +4,7 @@
 
 import React from 'react'
 import { render } from "react-dom";
+import 'stylesheets/custom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
